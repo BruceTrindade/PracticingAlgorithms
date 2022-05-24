41036264e89667ed71b101718c6af7508670bcaf
brucetrindade <brucetrindade2000@gmail.com>
2022-05-23 21:53:05 -0300

    Time to Jump

---
+++ app/src/main/java/com/example/metalball/TimeJump.kt
@@ -0,0 +1,16 @@
+package com.example.metalball
+
+import java.util.Scanner
+import kotlin.math.round
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    val d = reader.nextDouble()
+    val ds = reader.nextDouble()
+    var r = d/ds
+    val round = round(r)
+
+    if(r > round) {
+        println(round.toInt() + 1)
+    } else println(round.toInt())
+}
