6068ae0b6dd567dbd4c9a93bc9b6d00a2379ba14
brucetrindade <brucetrindade2000@gmail.com>
2022-06-01 18:48:18 -0300

    Reverted

---
+++ app/src/main/java/com/example/metalball/reverted.kt
@@ -0,0 +1,12 @@
+package com.example.metalball
+
+import java.util.*
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    val n = reader.nextInt()
+    var arrayint = Array<Int>(n) { reader.nextInt() }
+
+    println("${arrayint.reversed().toString().replace("[", "").replace("]", "").replace(",","")}")
+
+}
