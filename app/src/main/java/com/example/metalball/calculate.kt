4136b6d7f2db36321144c2669f418439a9c5a535
brucetrindade <brucetrindade2000@gmail.com>
2022-06-14 21:35:52 -0300

    calculate

---
+++ app/src/main/java/com/example/metalball/calculate.kt
@@ -0,0 +1,25 @@
+package com.example.metalball
+
+import java.util.Scanner
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    val c = reader.next()
+    val a = reader.nextDouble()
+    val b = reader.nextDouble()
+
+    operation(c,a,b)
+
+}
+
+fun operation(cc: String, aa: Double, bb: Double) {
+    var result = 0.0
+    when (cc) {
+        "+" -> result = aa + bb
+        "-" -> result = aa - bb
+        "/" -> result = aa / bb
+        "*" -> result = aa * bb
+    }
+    if (cc == "/")  return println(result)
+    else return  println("${String.format("%.0f",result)}")
+}
