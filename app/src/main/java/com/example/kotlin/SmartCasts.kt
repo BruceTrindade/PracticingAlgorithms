e079637b67a8e5497df2aa762d74d90fd2d97f7c
brucetrindade <brucetrindade2000@gmail.com>
2022-07-01 06:14:46 -0300

    Kotlin class

---
+++ app/src/main/java/com/example/kotlin/SmartCasts.kt
@@ -0,0 +1,35 @@
+package com.example.kotlin
+/*
+fun main() {
+    val x: String? = "2"
+
+    if (x is String) {
+        print(x.length) // x is automatically cast to String
+    }
+
+    val a: Int? = 2
+
+    if (a is Int) {
+        // 'a' is smart cast to Int
+        val b = a * 2 // b is also Int
+    }
+}*/
+
+fun main() {
+
+    // Smart Casting
+    val studentWithRealObject: Any? = Student()
+
+    if (studentWithRealObject is Student){
+        studentWithRealObject as Student
+        println("Student name: ${studentWithRealObject.name}")
+        println("Student age: ${studentWithRealObject.age}")
+    }
+
+}
+
+data class Student(
+    val name: String = "Bruce Trindade",
+    val age: Int = 27
+) {
+}
