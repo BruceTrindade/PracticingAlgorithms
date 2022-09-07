1ae55627eba863d8c3179543fc3535643b4edb7d
brucetrindade <brucetrindade2000@gmail.com>
2022-09-07 10:57:11 -0300

    bolo

---
+++ app/src/main/java/com/example/algorithms/nitro/midlevel/Cake.kt
@@ -0,0 +1,22 @@
+package com.example.algorithms.nitro.midlevel
+
+import java.util.Scanner
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    val a = reader.nextInt()
+    val b = reader.nextInt()
+    val c = reader.nextInt()
+
+    if (a < 2 || b < 3 || c < 5) println("0")
+    else {
+        val aa = a / 2
+        val bb = b / 3
+        val cc = c / 5
+        when {
+            aa <= bb && aa <= cc -> println(aa)
+            bb <= aa && bb <= cc -> println(bb)
+            else -> println(cc)
+        }
+    }
+}
