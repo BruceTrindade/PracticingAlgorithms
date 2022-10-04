7c25decdd0fce7c8565c6659ed059eb6a7d33652
brucetrindade <brucetrindade2000@gmail.com>
2022-10-04 07:06:29 -0300

    Highway

---
+++ app/src/main/java/com/example/algorithms/nitro/junior/Highway.kt
@@ -0,0 +1,21 @@
+package com.example.algorithms.nitro.junior
+
+import java.util.Scanner
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    val c = reader.nextInt()
+    val b = reader.next()
+    var up = 0
+
+    for (i in b.indices) {
+        when (b[i]) {
+            'P' -> up += 2
+            'C' -> up += 2
+            'A' -> up += 1
+            'D' -> up += 0
+        }
+    }
+
+    println(up)
+}
